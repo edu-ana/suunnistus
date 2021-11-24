8c6df81605cff446e86d7dfcd7b262c95c7bcd3e
edu-ana <94640165+edu-ana@users.noreply.github.com>
2021-11-24 23:14:00 +0200

    Update koodit.js

--- koodit.js
+++ koodit.js
@@ -1,11 +1,29 @@
 
 function testaaKoodi(koodi){
 switch(koodi) {
-  case "111":
-    var palaute = "Seuraava vihje kuuluu: Mistä kohdasta koulua näet seuraavat kohteet: A, C ja kasvi";
+  case "159723":
+    var palaute = "Etsi Alyssa";
     break;
-  case "112":
-    var palaute = "Koodi 112";
+  case "954367":
+    var palaute = "Etsi ruokalan tuoli X";
+    break;
+  case "436571":
+    var palaute = "Optiikan isä";
+    break;
+  case "127954":
+    var palaute = "Pelaa Anttia vastaan ja voita";
+    break;
+  case "669745":
+    var palaute = "Etsi kaappi numero 1010001";
+    break;
+  case "314685":
+    var palaute = "Missä kohtaa koulua pitää seistä, jotta näet XXXXXXXXX";
+    break;
+  case "765942":
+    var palaute = "Istu kohtaan 6,8 ja nauti elokuvasta.";
+    break;
+  case "116734":
+    var palaute = "Neljä huonetta piirtävät reitin";
     break;
   default:
     var palaute = "Väärä koodi";
