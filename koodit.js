98554e711a2bb87a0b66e42d4f3a4083c55acd32
edu-ana <94640165+edu-ana@users.noreply.github.com>
2024-11-10 21:40:32 +0200

    Update koodit.js

--- koodit.js
+++ koodit.js
@@ -2,7 +2,7 @@
 function testaaKoodi(koodi){
 switch(koodi) {
   case "Germanium":
-    var palaute = "Erinomaista! Olet ratkaissut suunnistustehtävän! Voit todistaa sen koodisanalla "diodi".";
+    var palaute = "Erinomaista! Olet ratkaissut suunnistustehtävän! Voit todistaa sen koodisanalla diodi.";
     break;
   default:
     var palaute = "Väärä koodi.";
